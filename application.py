1f215266334dc308459ec0f5459af1396222ff70
Avinash Varma <avinashvarma@Avinash-Air-Go.local>
2020-06-08 12:08:33 -0700

    Tuning Front End

--- application.py
+++ application.py
@@ -7,19 +7,25 @@ from flask.views import MethodView
 from index import Index
 from createtag import Todo
 
+
 print("Lost My Bag Website @lostmybag.com Started")
 
+
 application = flask.Flask(__name__)       # our Flask app
+
 
 application.add_url_rule('/',
                  view_func=Index.as_view('index'),
                  methods=["GET"])
 
+
 application.add_url_rule('/static/<path:filename>',endpoint='static',view_func=application.send_static_file) 
+
 
 application.add_url_rule('/createtag/',
                  view_func=Todo.as_view('createtag'),
                  methods=['GET', 'POST'])
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8000, debug=True)
