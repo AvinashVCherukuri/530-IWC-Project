2cc3155814504b7897c4c54ac5c32a3ebbd489df
Avinash Varma <avinashvarma@Avinash-Air-Go.local>
2020-06-08 19:40:04 -0700

    Index View Updated

--- index.py
+++ index.py
@@ -1,24 +1,7 @@
-from flask import render_template
+from flask import redirect, request, url_for, render_template
 from flask.views import MethodView
 import gbmodel
-import giphy_client
-from giphy_client.rest import ApiException
-import os
-import random
-
-API_KEY = os.environ['GIPHY_KEY']
-api_instance = giphy_client.DefaultApi()
 
 class Index(MethodView):
     def get(self):
-        model = gbmodel.get_model()
-        entries = [dict(username=row[0], bagcolor=row[1], cellphone=row[2], description=row[3], tagid=row[4], status=row[5] ) for row in model.select()]
-        offset = random.randint(0,50)        
-        try:
-            api_response = api_instance.gifs_search_get(API_KEY, 'welcome', limit=1, offset=offset, rating='g', lang='en', fmt='json')
-            if len(api_response.data) > 0:
-                embed_url = api_response.data[0].embed_url
-        except ApiException as e:
-            embed_url = ''
-
-        return render_template('index.html',entries=entries,embed_url=embed_url)
+        return render_template('index.html')
